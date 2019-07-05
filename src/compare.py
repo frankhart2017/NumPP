f583e525cbdf4515394dd0698b098125c9d9e2b9
frankhart2017 <sdharchou@gmail.com>
2019-07-06 00:23:51 +0530

    Partition

---
+++ src/compare.py
@@ -0,0 +1,12 @@
+import numpy as np
+import time
+
+start = time.time()
+
+a = np.array([100, 100, 100, 100, 100])
+
+stop = time.time()
+
+print(stop - start)
+
+# 6000 times faster than serial numpp
