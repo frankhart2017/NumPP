abe3b3e9815341b55d8164ceac2a07fc96563135
aviralkapoor <51873075+aviralkapoor@users.noreply.github.com>
2019-07-09 00:34:24 +0530

    For NXN arrays

--- src/main.c
+++ src/main.c
@@ -6,81 +6,77 @@ char s1[2],s2[2],s3[2];
 };
 int main()
 {
-    struct count m[18];
-    int a[3][3],b[3][3],i,j,c[18],k=0,l=0,p=0,q=0,r=0;
+    int a[3][3],b[3][3],i,j,k=0,l=0,p=0,q=0,r=-1,g,h,x=0,y=1;
+    printf("Enter the no. rows.\n");
+    scanf("%d",&g);
+    printf("Enter the no. columns.\n");
+    scanf("%d",&h);
+    int c[2*g*h];
+    struct count m[2*g*h];
     printf("Enter the elements of 1st array.\n");
-    for(i=0;i<3;i++)
+    for(i=0;i<g;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<h;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
     printf("Enter the elements of 2nd array.\n");
-    for(i=0;i<3;i++)
+    for(i=0;i<g;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<h;j++)
         {
             scanf("%d",&b[i][j]);
         }
     }
-    for(i=0;i<3;i++)
+    for(i=0;i<g;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<h;j++)
         {
             c[k]=a[i][j];
             k++;
         }
     }
-    for(i=0;i<3;i++)
+    for(i=0;i<g;i++)
     {
-        for(j=0;j<3;j++)
+        for(j=0;j<h;j++)
         {
             c[k]=b[i][j];
             k++;
         }
     }
-    for(i=0;i<18;i++)
+    for(i=0;i<2*g*h;i++)
     {
-      if(i>2&&i<=5)
+      if(i>=x*h&&i<y*h&&i!=g*h)
       {
-        q=1;
-        r=0;
+        r++;
       }
-      if(i>5&&i<=8)
+      else if(i==g*h)
       {
-          q=2;
-          r=0;
-      }
-      if(i>8&&i<=11)
-      {
-          p=1;
+          p++;
           q=0;
           r=0;
+          x++;
+          y++;
+
       }
-      if(i>11&&i<=14)
+      else
       {
-          p=1;
-          q=1;
-          r=0;
-      }
-      if(i>14&&i<=17)
-      {
-          p=1;
-          q=2;
-          r=0;
+        x++;
+        y++;
+        q++;
+        r=0;
       }
       itoa(p,m[l].s1,10);
       itoa(q,m[l].s2,10);
       itoa(r,m[l].s3,10);
-      r++;
       l++;
-      }
+    }
       l=0;
       printf("The linear arranged array is->\n");
-      for(i=0;i<18;i++)
+      for(i=0;i<2*g*h;i++)
       {
-          printf("%d  Str Code is-%s%s%s\n",c[i],m[l].s1,m[l].s2,m[l].s3);
+          printf("%d  Str Code is %s%s%s\n",c[i],m[l].s1,m[l].s2,m[l].s3);
           l++;
       }
       return 0;
