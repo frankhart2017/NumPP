4bbba8065b7e225112cce4edf431cee0b95dede3
aviralkapoor <51873075+aviralkapoor@users.noreply.github.com>
2019-08-10 14:02:43 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,21 +9,24 @@ using namespace std::chrono;
 
 int main() {
 
-  numpp<int> np({2, 2});
+  numpp<int> np({2,2});
   numpp<int> np1({2,2});
+  numpp<int> np2({2,2});
+  numpp<int> np3({2,2});
 
   auto start = high_resolution_clock::now();
 
-  np.push_vec({2, 2}, {1, 2, 3, 4});
-  np1.push_vec({2, 2}, {5, 4, 7, 8});
-
+  np.push_vec({2,2}, {1,2,3,4});
+  np1.push_vec({2,2}, {5,4,7,8});
+  np2.push_vec({2,2}, {4,2,6,7});
+  np3.push_vec({2,2}, {0,0,0,0});
+  
   auto stop = high_resolution_clock::now();
   auto duration = duration_cast<nanoseconds>(stop - start);
-
   cout<<duration.count()<<endl;
-
+  
   np.shape();
-  np.add(np1.data);
-  np.sub(np1.data);
+  np3.add(np1,np2);
+  np3.sub(np1,np2);
   cout<<endl;
 }
